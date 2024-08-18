54affea990b5f76a4a8c7380fbcf88f9e2544074
elgikore <elgienbertc@gmail.com>
2024-08-18 15:34:55 +1200

    Published this project

---
+++ BinomialApp.py
@@ -0,0 +1,208 @@
+import sys
+import os
+from BinomialDistribution import BinomialDist
+from typing import Callable
+from tabulate import tabulate
+
+binomial = None
+
+def clear() -> None:
+    # Works well in VSCode; from: https://stackoverflow.com/questions/517970/how-can-i-clear-the-interpreter-console
+    # The Console.Clear() equivalent in Python
+    print("\033[H\033[J", end='')
+
+    # In case escape codes aren't supported
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def clear_continue() -> None:
+    input("Press ENTER to continue. ")
+    clear()
+
+def not_in_selection() -> None:
+    print("Not in selection!", end=' ')
+    clear_continue() 
+
+def end_screen() -> None:
+    print('See you soon! Press ENTER to quit. ', end=' ')
+    input()
+    sys.exit(0)
+
+def header(title_name: str, symbol: str = '=', equals_length: int = 80) -> None:
+    print(f"{symbol * equals_length}")
+    print(f"{title_name.center(equals_length)}")
+    print(f"{symbol * equals_length}")
+
+def title() -> None:
+    header("Binomial Distribution Calculator")
+    print("             / n \\      x         n - x")
+    print("Pr(X = x) = |     | (pi)  (1 - pi)")
+    print("             \\ x /")
+    print("\nWhere:")
+    print("\t- n = Total number of independent trials")
+    print("\t- pi = Probability of success/target outcome")
+    print("\t- x = Probability of x successes (i.e. the little x in P(X = x))")
+    print("Conditions:")
+    print("\t- Event can be summarized to two outcomes (i.e. pi and its complement)")
+    print("\t- Trials (n) must be independent")
+    print("\t- Trials (n) must be fixed prior to data collection")
+    print("\t- Probability of success/target outcome (pi) must stay constant\n")
+
+def n_and_pi(header_screen: Callable) -> tuple[int, float]:
+    while True:
+        header_screen()
+
+        print("NOTE: Type Q/q to quit.\n")
+
+        try:
+            n = input("What is the value of n? (Must be a whole number): ")
+            if (n.lower() == 'q'): end_screen()
+            n = int(n)
+
+            pi = input("What is the value of pi?: ")
+            if (pi.lower() == 'q'): end_screen()
+            pi = float(pi)
+        except ValueError as val_err:
+            print(f"Invalid input! ({val_err})", end=' ')
+            clear_continue()
+            continue
+        else:
+            return (n, pi)
+
+
+def selection(header_screen: Callable, selection_list: tuple) -> int:
+    selection_list_len = len(selection_list)
+    
+    while True:
+        header_screen()
+        print("What do you want to do?")
+        for num, selection in enumerate(selection_list):
+            print(f"({num + 1}) {selection}")
+        print("(0) Quit")
+
+        try:
+            selection = int(input(f"\nPlease enter a number between 0 and {selection_list_len}: "))
+            
+            if (selection < 0 or selection > selection_list_len): not_in_selection()
+        except ValueError as val_err:
+            print(f"Invalid input! ({val_err})", end=' ')
+            clear_continue()
+            continue
+        else:
+            return selection
+
+def graph_selection(header_screen: Callable, graph_selections: tuple) -> None:
+    while True:
+        match selection(header_screen, graph_selections):
+            case 0: 
+                clear_continue()
+                break
+            case 1: 
+                binomial.show_graph('bar')
+                clear_continue()
+            case 2: 
+                binomial.show_graph('line')
+                clear_continue()
+            case 3: 
+                binomial.show_graph('scatter')
+                clear_continue()
+            case 4:
+                binomial.show_graph('linepoint')
+                clear_continue()
+            case 5:
+                binomial.show_graph('linebar')
+                clear_continue()
+            case _: 
+                continue
+
+def probability_operations(header_screen: Callable, pr_selections: tuple) -> None:
+    while True:
+        match selection(header_screen, pr_selections):
+            case 0: 
+                clear_continue()
+                break
+            case 1:
+                clear()
+                x = set_x_value(pr_selections[0])
+                print(f"Pr(X <= x) = {binomial.less_or_equal(x)}\n")
+                clear_continue()
+            case 2:
+                clear()
+                x = set_x_value(pr_selections[1])
+                print(f"Pr(X >= x) = {binomial.greater_or_equal(x)}\n")
+                clear_continue()
+            case 3:
+                clear()
+                x = set_x_value(pr_selections[2])
+                print(f"Pr(X < x) = {binomial.lesser_than(x)}\n")
+                clear_continue()
+            case 4:
+                clear()
+                x = set_x_value(pr_selections[3])
+                print(f"Pr(X > x) = {binomial.greater_than(x)}\n")
+                clear_continue()
+            case 5:
+                clear()
+                x = set_x_value(pr_selections[4])
+                print(f"Pr(X = x) = {binomial.small_x_value(x)}\n")
+                clear_continue()
+            case _:
+                continue
+
+def set_x_value(header_title: str) -> int:
+    total_x_instances = len(binomial.probability_table) - 1
+
+    while True:
+        header(header_title, '-')
+        print("NOTE: Type Q/q to quit.\n")
+
+        try:
+            x = input(f"What is the value of x (i.e small x)? Must be between 0 and {total_x_instances}: ")
+            if (x.lower() == 'q'): 
+                clear_continue()
+                break
+
+            x = int(x)
+            
+            if (x < 0 or x > total_x_instances): 
+                not_in_selection()
+                continue
+        except ValueError as val_err:
+            print(f"Invalid input! ({val_err})", end=' ')
+            clear_continue()
+            continue
+        else:
+            return x
+
+if __name__ == "__main__":
+    MAIN_MENU_SELECTIONS = ("Operations on Pr(X = x)", "Graph", "Summary", "Change x and pi")
+    GRAPH_SELECTIONS = ("Bar Graph", "Line Graph", "Scatterplot", "Line Graph with Points", "Line and Bar Graph")
+    PR_SELECTIONS = ("Pr(X <= x)", "Pr(X >= x)", "Pr(X < x)", "Pr(X > x)", "Pr(X = x)")
+    
+    # Title Screen
+    n_pi = n_and_pi(title)
+    binomial = BinomialDist(n_pi[0], n_pi[1])
+    clear()
+    
+    # Main menu
+    while True:
+        match selection(lambda: header("Main Menu"), MAIN_MENU_SELECTIONS):
+            case 0: end_screen()
+            case 1:
+                clear()
+                probability_operations(lambda: header(MAIN_MENU_SELECTIONS[0], '*'), PR_SELECTIONS)
+            case 2:
+                clear()
+                graph_selection(lambda: header(MAIN_MENU_SELECTIONS[1], '*'), GRAPH_SELECTIONS)
+            case 3:
+                clear()
+                header(MAIN_MENU_SELECTIONS[2], '*')
+                print(tabulate(binomial.probability_table, headers="keys", showindex=False, tablefmt="double_grid"), '\n')
+                print(binomial, '\n')
+                clear_continue()
+            case 4:
+                clear()
+                n_pi = n_and_pi(lambda: header(MAIN_MENU_SELECTIONS[3], '*'))
+                binomial = BinomialDist(n_pi[0], n_pi[1])
+                clear_continue()
+            case _:
+                continue
---
+++ BinomialDistribution.py
@@ -0,0 +1,310 @@
+import numpy as np
+import math
+import pandas as pd # for table
+
+# for graphs
+import seaborn as sns
+from matplotlib import pyplot as plt 
+
+class BinomialDist:
+    """
+        This class deals with the binomial distribution, its calculation and graphs.
+
+        Attributes
+        ----------
+        n: int
+            Number of trials.
+        pi: float or int
+            Probability of success/target of interest.
+        probability_table: np.array
+            A 2D array (table) containing the x's and its probabilities.
+    """
+
+    def __init__(self, n: int, pi: float | int):
+        """
+            Initializes the values of n and pi.
+
+            Parameters
+            ----------
+            n: int
+                Number of trials.
+            pi: float or int
+                Probability of success/target of interest
+        """
+        print()
+        self.__n = n
+        self.__pi = np.longdouble(pi)
+
+        # Probability array generation
+        self.__probability_table = self.__generate_table()
+    
+
+
+    # Table Generation
+    def __generate_table(self) -> np.array:
+        """Generates a probability table as an `np.array` for decimal precision."""
+        probability_table = np.zeros(shape=(self.__n + 1, 2))
+        
+        for i in range(len(probability_table)):
+            probability_table[i] = np.array([i, self.__success_probability(i)])
+        
+        return probability_table
+    
+    def __success_probability(self, small_x: int) -> np.longdouble:
+        """
+            Basically the binomial distribution formula.
+
+            Parameters
+            ----------
+            small_x: int
+                The x value to calculate the probability with.
+            
+            Returns
+            -------
+            Probability of x (`small_x`), i.e. Pr(X = x).
+        """
+        # Combination formula
+        nCr = math.factorial(self.__n) / (math.factorial(small_x) * math.factorial(self.__n - small_x))
+
+        return np.longdouble(nCr * (self.__pi ** small_x) * ((1 - self.pi) ** (self.__n - small_x)))
+
+
+
+    # Getters and Setters
+    @property
+    def n(self) -> int:
+        """Retrieves the n value."""
+        return self.__n
+    
+    @n.setter
+    def n(self, new_n: int) -> None:
+        """
+            Modifies the n value.
+            
+            Parameters
+            ----------
+            new_n: int
+                New n value.
+        """
+        self.__n = new_n
+        self.__probability_table = self.__generate_table() # Remake table
+    
+    @property
+    def pi(self) -> np.longdouble:
+        """Retrieves the pi value."""
+        return self.__pi
+    
+    @pi.setter
+    def pi(self, new_pi: float) -> None:
+        """
+            Modifies the pi value.
+
+            Parameters
+            ----------
+            new_pi: float
+                New pi value.
+        """
+        self.__pi = np.longdouble(new_pi)
+        self.__probability_table = self.__generate_table()
+
+    @property
+    def probability_table(self) -> pd.DataFrame:
+        """
+            Returns the calculated probability table as a dataframe.
+
+            Returns
+            -------
+            A dataframe of probabilities (`pd.Dataframe`)
+        """
+        return pd.DataFrame({
+            '(small) x': self.__probability_table[:, 0].astype(int),
+            'Probability': self.__probability_table[:, 1]
+        })
+    
+
+
+    # Graph
+    def show_graph(self, graph_type: str) -> None:
+        """
+            Graph with specified type.
+
+            Parameters
+            ----------
+            graph_type: str
+                The type of graph specified, either:
+                * `'bar'` for bar graph.
+                * `'line'` for line graph.
+                * `'scatter'` for scatter graph.
+                * `'linepoint'` for line graph with visible data points.
+                * `'linebar'` for line and bar graph.
+            
+            Returns
+            -------
+            `None`
+
+            Throws
+            ------
+            `ValueError` if `graph_type` isn't one of the valid graph types.
+        """
+        sns.set_style("whitegrid")
+
+        match graph_type.lower():
+            case 'bar':
+                ax = sns.barplot(self.probability_table, x='small_x', y='Probability', color='#3A1078')
+            case 'line':
+                ax = sns.lineplot(self.probability_table, x='small_x', y='Probability', c='#4E31AA', markers=True, dashes=False)
+            case 'scatter':
+                ax = sns.scatterplot(self.probability_table, x='small_x', y='Probability', c='#3795BD')
+            case 'linepoint':
+                ax = sns.scatterplot(self.probability_table, x='small_x', y='Probability', c='#3795BD')
+                sns.lineplot(self.probability_table, x='small_x', y='Probability', c='#4E31AA', markers=True, dashes=False)
+            case 'linebar':
+                ax = sns.barplot(self.probability_table, x='small_x', y='Probability', color='#3A1078')
+                sns.lineplot(self.probability_table, x='small_x', y='Probability', c='#3795BD', markers=True, dashes=False)
+            case _:
+                raise ValueError(f'Invalid input! "{graph_type}" is not a valid graph type.')
+        
+        plt.suptitle("Probability Distribution", x = 0.125, y = 0.94, ha="left", fontweight="bold", size="xx-large")
+        plt.title(f"n = {self.__n}, $\\pi$ = {self.__pi}", x = 1, ha="right", fontweight="light", style="italic", size="small", c="#808080")
+        
+        ax.set(xlabel='Possible Values (X)', ylabel='Probability of X')
+        plt.show()
+    
+
+
+    # Probabiltiy Operations. small_x is basically x in this context, to distunguish it from capital X.
+    def less_or_equal(self, small_x: int) -> np.longdouble:
+        """
+            Equivalent to Pr(X <= x).
+
+            Parameters
+            ----------
+            target: int
+                `small_x` (x) in Pr(X <= x).
+
+            Returns
+            -------
+            small_x: np.longdouble
+                Pr(X <= x) i.e. sum of all probabilities from Pr(X = 0) to Pr(X = x).
+        """
+        return np.longdouble(np.sum(self.__probability_table[:(small_x + 1), 1]))
+    
+    def greater_or_equal(self, small_x: int) -> np.longdouble:
+        """
+            Equivalent to Pr(X >= x).
+
+            Parameters
+            ----------
+            target: int
+                `small_x` (x) in Pr(X >= x).
+
+            Returns
+            -------
+            small_x: np.longdouble
+                Pr(X >= x) i.e. sum of all probabilities from Pr(X = 0) to Pr(X = x) and getting 
+                its complement.
+        """
+        return 1 - self.less_or_equal(small_x)
+    
+    def lesser_than(self, small_x: int) -> np.longdouble:
+        """
+            Equivalent to Pr(X < x).
+
+            Parameters
+            ----------
+            target: int
+                `small_x` (x) in Pr(X < x).
+
+            Returns
+            -------
+            small_x: np.longdouble
+                Pr(X < x) i.e. sum of all probabilities from Pr(X = 0) to Pr(X = (x - 1)).
+        """
+        return np.longdouble(np.sum(self.__probability_table[:small_x, 1]))
+    
+    def greater_than(self, small_x: int) -> np.longdouble:
+        """
+            Equivalent to Pr(X > x).
+
+            Parameters
+            ----------
+            target: int
+                `small_x` (x) in Pr(X > x).
+
+            Returns
+            -------
+            small_x: np.longdouble
+                Pr(X > x) i.e. sum of all probabilities from Pr(X = 0) to Pr(X = (x - 1)) and getting 
+                its complement. 
+        """
+        return 1 - self.lesser_than(small_x)
+    
+    def small_x_value(self, target: int) -> np.longdouble:
+        """
+            Equivalent to Pr(X = x).
+
+            Parameters
+            ----------
+            target: int
+                `small_x` (x) in Pr(X = x).
+
+            Returns
+            -------
+            small_x: np.longdouble
+                Pr(X = x).
+            
+            Throws
+            ------
+            `IndexError` if the `x` value exceeds n
+        """
+        if target > (len(self.__probability_table) - 1): 
+            raise IndexError(f"Outside of range 0 to {len(self.__probability_table) - 1}.")
+
+        return np.longdouble(self.__probability_table[target, 1])
+    
+
+
+    # Mean, variance, and standard deviation
+    def binom_mean(self) -> np.longdouble:
+        """
+            Mean of the binomial distribution.
+
+            Returns
+            -------
+            `np.longdouble`
+        """
+        return self.__n * self.__pi
+    
+    def binom_var(self) -> np.longdouble:
+        """
+            Variance of the binomial distribution.
+
+            Returns
+            -------
+            `np.longdouble`
+        """
+        return self.binom_mean() * (1 - self.__pi)
+    
+    def binom_sdev(self) -> np.longdouble:
+        """
+            Standard deviation of the binomial distribution.
+
+            Returns
+            -------
+            `np.longdouble`
+        """
+        return np.sqrt(self.binom_var()) 
+    
+
+    
+    def __str__(self) -> str:
+        """
+            Displays the attribute values, as well as the mean, variance,
+            and standard deviation.
+        """
+        
+        return f'n\t\t{self.__n}\n' \
+                f'pi\t\t{self.__pi}\n' \
+                f'Mean\t\t{self.binom_mean()}\n' \
+                f'Variance\t{self.binom_var()}\n' \
+                f'Std. dev.\t{self.binom_sdev()}'
